15d012964058db54b9b2a61e65ab7f905dd6d206
jRicardoLima <j.ricardo_lima@outlook.com>
2023-02-21 00:23:56 -0300

    first commit

---
+++ bin/desafio_imc_app.dart
@@ -0,0 +1,5 @@
+import 'package:desafio_imc_app/desafio_imc_app.dart' as desafio_imc_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desafio_imc_app.calculate()}!');
+}
---
+++ test/desafio_imc_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:desafio_imc_app/desafio_imc_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
